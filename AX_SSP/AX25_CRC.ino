be7c54a48345f585ce67ddb99ca723501697ed9f
mohamedezzat99 <ezzatmohamed516@gmail.com>
2022-06-21 20:21:24 +0200

    Added wireless interface file for LabVIEW

--- AX_SSP/AX25_CRC.ino
+++ AX_SSP/AX25_CRC.ino
@@ -5,7 +5,6 @@
 
 #include "AX25_CRC.h"
 
-
 /*--------------------------------------------------------------------------*
  * AX.25 FCS calculation (bitwise method).
  * CRC-16-CCITT G(x) = x16 + x12 + x5 + 1.
@@ -19,31 +18,35 @@
  * the CRC with a final XORed operation.
  *--------------------------------------------------------------------------*/
 
-uint16 computeCRC( uint8* data_p, uint16 *length) {
-  unsigned char x;
-  unsigned short crc = 0xFFFF;
-  uint16 len;
-  len = *length;
-  uint8 data_copy;
-  while (len--) {
-      data_copy = *data_p;
-    /*  reverse the bits in each 8-bit byte going in */
-    data_copy = (data_copy & 0x55555555) << 1 | (data_copy & 0xAAAAAAAA) >> 1;
-    data_copy = (data_copy & 0x33333333) << 2 | (data_copy & 0xCCCCCCCC) >> 2;
-    data_copy = (data_copy & 0x0F0F0F0F) << 4 | (data_copy & 0xF0F0F0F0) >> 4;
+uint16 computeCRC(uint8 *data_p, uint16 *length) {
+	unsigned char x;
+	unsigned short crc = 0xFFFF;
+	uint16 len;
+	len = *length;
+	uint8 data_copy;
+	while (len--) {
+		data_copy = *data_p;
+		/*  reverse the bits in each 8-bit byte going in */
+		data_copy = (data_copy & 0x55555555) << 1
+				| (data_copy & 0xAAAAAAAA) >> 1;
+		data_copy = (data_copy & 0x33333333) << 2
+				| (data_copy & 0xCCCCCCCC) >> 2;
+		data_copy = (data_copy & 0x0F0F0F0F) << 4
+				| (data_copy & 0xF0F0F0F0) >> 4;
 
-    x = crc >> 8 ^ data_copy;
-    data_p++;
-    x ^= x >> 4;
-    crc = (crc << 8) ^ ((unsigned short)(x << 12)) ^ ((unsigned short)(x << 5)) ^ ((unsigned short)x);
-  }
-  /*reverse the 16-bit CRC*/
-  crc = (crc & 0x55555555) << 1 | (crc & 0xAAAAAAAA) >> 1;
-  crc = (crc & 0x33333333) << 2 | (crc & 0xCCCCCCCC) >> 2;
-  crc = (crc & 0x0F0F0F0F) << 4 | (crc & 0xF0F0F0F0) >> 4;
-  crc = (crc & 0x00FF00FF) << 8 | (crc & 0xFF00FF00) >> 8;
+		x = crc >> 8 ^ data_copy;
+		data_p++;
+		x ^= x >> 4;
+		crc = (crc << 8) ^ ((unsigned short) (x << 12))
+				^ ((unsigned short) (x << 5)) ^ ((unsigned short) x);
+	}
+	/*reverse the 16-bit CRC*/
+	crc = (crc & 0x55555555) << 1 | (crc & 0xAAAAAAAA) >> 1;
+	crc = (crc & 0x33333333) << 2 | (crc & 0xCCCCCCCC) >> 2;
+	crc = (crc & 0x0F0F0F0F) << 4 | (crc & 0xF0F0F0F0) >> 4;
+	crc = (crc & 0x00FF00FF) << 8 | (crc & 0xFF00FF00) >> 8;
 
-  return crc;
+	return crc;
 }
 /*--------------------------------------------------------------------------*
  * AX.25 FCS positioning.
@@ -55,14 +58,14 @@ uint16 computeCRC( uint8* data_p, uint16 *length) {
  * *OpArrSize     it stores the index of the last inserted element in the array to keep track of size
  *--------------------------------------------------------------------------*/
 void AX25_putCRC(uint8 *buffer, uint16 *OpArrSize) {
-  uint16 crc;
+	uint16 crc;
 
-  /* FCS calculation. */
-  crc = computeCRC(buffer, OpArrSize);
+	/* FCS calculation. */
+	crc = computeCRC(buffer, OpArrSize);
 
-  /* Put the FCS in the right place with the 15th bit to be sent first. */
-  buffer[*OpArrSize] = ((crc >> 8) & 0xff);
-  *OpArrSize=*OpArrSize+1;
-  buffer[*OpArrSize] = (crc & 0xff);
-  *OpArrSize=*OpArrSize+1;
+	/* Put the FCS in the right place with the 15th bit to be sent first. */
+	buffer[*OpArrSize] = ((crc >> 8) & 0xff);
+	*OpArrSize = *OpArrSize + 1;
+	buffer[*OpArrSize] = (crc & 0xff);
+	*OpArrSize = *OpArrSize + 1;
 }
